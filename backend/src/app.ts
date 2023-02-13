0c1877d9b7af30b3cf713880db0b59f6b0bb3b1a
unknown <souhail7ouabou@gmail.com>
2023-02-13 01:00:09 +0100

    setup mongoose

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import express from 'express'
+import NoteModel from './models/note'
+const app = express()
+
+app.get('/', async(req, res) => {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+
+})
+
+export default app
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
-import express from  'express';
-const app = express()
-const port = 5000;
+import app from './app'
+import enc from './util/validateEnv'
+import mongoose from 'mongoose'
 
-app.get('/',(req,res)=>{
-res.send("Hiiii")
-})
-app.listen(port,()=>{
-    console.log("Server running on port : "+port);
-})
+//1. CONNECT MOGOOSE TO OUR DB MONGODB
+//we cant use async await in top level thats why we use then
+const port = enc.PORT
+mongoose
+    .connect(enc.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log('Mongoose Connected')
+        app.listen(port, () => {
+            console.log('Server running on port : ' + port)
+        })
+    })
+    .catch(console.error)
