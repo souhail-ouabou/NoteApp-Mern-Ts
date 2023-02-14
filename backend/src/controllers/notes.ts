fe7c6994d0f156611a737abf6dc2f048144fccf6
unknown <souhail7ouabou@gmail.com>
2023-02-15 00:03:40 +0100

    Expres Http Error Handling

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,4 +1,6 @@
 import { RequestHandler } from 'express'
+import createHttpError from 'http-errors'
+import mongoose from 'mongoose'
 import NoteModel from '../models/note'
 
 export const getNotes: RequestHandler = async (req, res, next) => {
@@ -12,24 +14,44 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 }
 export const getNote: RequestHandler = async (req, res, next) => {
     try {
-        const id = req.params.noteId;
-        const note = await NoteModel.findById(id).exec();
+        const noteId = req.params.noteId
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, 'Invalid Note id')
+        }
+        const note = await NoteModel.findById(noteId).exec()
+
+        if (!note) {
+            throw createHttpError(400, 'Note Not found')
+        }
 
         res.status(200).json(note)
     } catch (error) {
         next(error)
     }
 }
-export const createNote: RequestHandler = async (req, res, next) => {
-    const { title, text } =req.body
+
+//what type of variables we expect in body
+interface CreateNoteBody {
+    title?: string
+    text?: string
+}
+export const createNote: RequestHandler<
+    unknown,
+    unknown,
+    CreateNoteBody,
+    unknown
+> = async (req, res, next) => {
+    const { title, text } = req.body
     try {
+        if (!title) {
+            throw createHttpError(400, 'The Note must have a title')
+        }
         const newNote = await NoteModel.create({
-            title:title,
-            text:text,
-        });
+            title: title,
+            text: text,
+        })
         res.status(201).json(newNote)
     } catch (error) {
         next(error)
     }
 }
-
